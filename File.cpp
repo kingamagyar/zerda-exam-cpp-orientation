1679b54409f98d5cb8bbe59ffc39fe37b2c5fa5b
Kinga Magyar <kinga.magyar89@gmail.com>
2017-01-11 13:10:11 +0100

    Corrected one return at a }

--- File.cpp
+++ File.cpp
@@ -7,21 +7,21 @@ std::string File::read_in_file(std::string input_file_name) {
   myfile.open(input_file_name);
   std::string buffer;
   if (!myfile.is_open()) {
-    throw "Error: your file is not open!\n";
+    std::cout << "Error: your file is not open!\n";
   } else {
     while (getline(myfile, buffer)) {
       content += buffer + "\n";
     }
     myfile.close();
-    return content;
   }
+  return content;
 }
 
 void File::write_to_file(std::string output_file_name, std::string content) {
   std::ofstream myfile;
   myfile.open (output_file_name);
   if (!myfile.is_open()) {
-    throw "Error: the file is not open\n";
+    std::cout << "Error: the file is not open\n";
   } else {
     myfile << content;
     myfile.close();
@@ -32,7 +32,7 @@ void File::print_content_to_screen(std::string input_file_name) {
   std::ifstream myfile;
   myfile.open(input_file_name);
   if (!myfile.is_open()) {
-    throw "Error: the file is not open\n";
+    std::cout << "Error: the file is not open\n";
   } else {
     while (getline(myfile, content)) {
       std::cout << content << std::endl;
